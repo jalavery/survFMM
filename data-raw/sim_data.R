8b2c006095bca7cada1d059073fc581292ad8885
jalavery <laveryj@mskcc.org>
2026-04-27 08:36:43 -0400

    Add covariates to simulated dataset

--- data-raw/sim_data.R
+++ data-raw/sim_data.R
@@ -6,11 +6,14 @@ sim_data <- S1172_2156_32112_TxIPTW2Opp4_LStr_E30_N1500 %>%
   filter(repetition == 1) %>%
   unnest(data) %>%
   rename(covariate_sim_normal = covariate_sim_tmb_zscore,
+         covariate_sim_binary1 = covariate_sim_sex_binary,
+         covariate_sim_binary2 = covariate_sim_histology_binary,
          event_status = pfs_status,
          time_to_event_days = tt_pfs_days) %>%
   select(-repetition, -setting_name) %>%
   select(record_id, latent_subgroup,
-         covariate_sim_normal, tx,
+         covariate_sim_normal, covariate_sim_binary1, covariate_sim_binary2,
+         tx,
          time_to_event_days, event_status,
          iptw_trim97, ipcw_trim97,
          iptw_ipcw_trim97)
