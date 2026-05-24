1bb21df1c424a54a9a7994740b6fe9a13b843c6a
jalavery <laveryj@mskcc.org>
2026-05-24 09:56:26 -0400

    Update clean_subgroup_labels to clean >2 subgroups
    Also includes update to tests to use setup file and updates to add .data terms

--- R/clean_subgroup_labels.R
+++ R/clean_subgroup_labels.R
@@ -21,72 +21,110 @@ clean_subgroup_labels <- function(survFMM_object,
   # get list of tidied outcome models
   tidy_outcome_models <- names(survFMM_object)[grepl("final_outcome_model_tidy_", names(survFMM_object))]
 
-  # currently only available for k = 2
-  if (max(purrr::pluck(survFMM_object, "final_df")$k)>2){
-    stop("Subgroup label alignment is currently only available for 2 subgroups.")
-  }
-
   # if tx term doesn't exist
   if (!any(grepl(tx_term, purrr::pluck(survFMM_object, tidy_outcome_models[1])$term))){
     stop("Check that the `tx_term` input exists in the data and matches the tx term supplied for the survFMM call.")
   }
+
+  # browser()
 
   # determine which subgroup had the higher tx term
   subgroup_order <- survFMM_object %>%
     # keep results objects corresponding to the outcome models
     purrr::keep_at(tidy_outcome_models) %>%
     # identify which subgroup
-    purrr::imap(., ~dplyr::mutate(.x, k = as.numeric(str_remove(pattern = "final_outcome_model_tidy_",
+    purrr::imap(., ~dplyr::mutate(.x, k = as.numeric(stringr::str_remove(pattern = "final_outcome_model_tidy_",
                                                   string = .y)))) %>%
-    bind_rows() %>%
-    dplyr::filter(term == tx_term) %>%
-    dplyr::arrange(estimate) %>%
-    dplyr::mutate(k_clean = 1:n())
+    dplyr::bind_rows() %>%
+    dplyr::filter(.data$term == tx_term) %>%
+    dplyr::arrange(.data$estimate) %>%
+    dplyr::mutate(k_clean = 1:dplyr::n())
 
   # determine if re-ordering is required ---------------------------------
   reorder_flag <- subgroup_order %>%
-    dplyr::filter(k != k_clean) %>%
+    dplyr::filter(.data$k != .data$k_clean) %>%
     nrow() > 0
 
   # correct label switching ---------------------------------
   if (reorder_flag == TRUE){
 
+    # flip probabilities in final_df
+    final_df <- purrr::pluck(survFMM_object, "final_df") %>%
+      dplyr::rename(assn_subgroup_orig = assn_subgroup,
+                    k_orig = k) %>%
+      # merge on updated subgroup labels for k
+      dplyr::left_join(.,
+                       subgroup_order %>%
+                         dplyr::select(k, k_clean) %>%
+                         dplyr::mutate(k = as.character(k)),
+                       by = c("k_orig" = "k")) %>%
+      dplyr::mutate(k = k_clean) %>%
+      # now merge updated subgroup labels on for assn_subgroup
+      dplyr::left_join(.,
+                       subgroup_order %>%
+                         dplyr::select(k, k_clean),
+                       by = c("assn_subgroup_orig" = "k")) %>%
+      dplyr::mutate(assn_subgroup = k_clean.y) %>%
+      dplyr::select(-contains("k_clean"), -k_orig, -assn_subgroup_orig)
+
     # get list of old vs new subgroup names based on k and k_clean
-    rename_list <- tidyr::tibble(original = paste0("posterior_prob", subgroup_order$k),
-                          new = paste0("posterior_prob", subgroup_order$k_clean))
+    rename_list <- tidyr::tibble(
+      original = paste0("posterior_prob", subgroup_order$k),
+      new = paste0("posterior_prob", subgroup_order$k_clean))
 
     # subgroup assignment
-    subgroup_assn <- purrr:::pluck(survFMM_object, "subgroup_assn") %>%
+    subgroup_assn <- purrr::pluck(survFMM_object, "subgroup_assn") %>%
       dplyr::rename_with(.fn = ~rename_list$new, .cols = rename_list$original) %>%
       dplyr::rename(assn_subgroup_original = assn_subgroup) %>%
       # merge on clean subgroup assignment
       dplyr::left_join(subgroup_order %>% dplyr::select(k, k_clean),
                 by = c("assn_subgroup_original" = "k")) %>%
       dplyr::rename(assn_subgroup = k_clean) %>%
-      # latent_sugroup only known in simulations, but want to keep it if it's available
-      dplyr::select(record_id, starts_with("latent_subgroup"), assn_subgroup, paste0("posterior_prob", 1:max(subgroup_order$k)))
+      # latent_subgroup only known in simulations, but want to keep it if it's available
+      dplyr::select(record_id, dplyr::starts_with("latent_subgroup"),
+                    assn_subgroup, paste0("posterior_prob", 1:max(subgroup_order$k)))
 
-    # subgroup model
-    final_subgroup_model_tidy <- purrr::pluck(survFMM_object, "final_subgroup_model_tidy") %>%
-      dplyr::rename(conf.low_orig = conf.low,
-             conf.high_orig = conf.high) %>%
-      dplyr::mutate(estimate = -estimate,
-             conf.low = -conf.high_orig,
-             conf.high = -conf.low_orig,
-             y.level = 2)
+    # subgroup model: use final_df created above with corrected levels
+    final_subgroup_model <- (nnet::multinom(k ~ . - posterior_prob,
+                                            data = final_df %>%
+                                              dplyr::select(
+                                                k,
+                                                tidyr::all_of(
+                                                  labels(terms(purrr::pluck(survFMM_object, "final_subgroup_model")))
+                                                ),
+                                                posterior_prob
+                                              ),
+                                            weights = posterior_prob,
+                                            trace = FALSE
+    ))
 
-    # flip probabilities in final_df
-    final_df <- purrr::pluck(survFMM_object, "final_df") %>%
-      dplyr::rename(assn_subgroup_orig = assn_subgroup,
-             k_orig = k) %>%
-      dplyr::mutate(assn_subgroup = case_when(
-        assn_subgroup_orig == 1 ~ 2,
-        assn_subgroup_orig == 2 ~ 1
-      ),
-      k = case_when(
-        k_orig == 1 ~ as.character(2),
-        k_orig == 2 ~ as.character(1)
-      ))
+    # tidy updated subgroup model
+    final_subgroup_model_tidy <- broom::tidy(final_subgroup_model, conf.int = TRUE)
+
+    # subgroup model covariance matrix
+    final_subgroup_model_cov_mtx <- unname(stats::vcov(final_subgroup_model))
+
+    # outcome model objects
+    final_outcome_model_rename_list <- tidyr::tibble(
+      original = paste0("final_outcome_model_tidy_", subgroup_order$k),
+      new = paste0("final_outcome_model_tidy_", subgroup_order$k_clean)) %>%
+      dplyr::arrange(original)
+
+    final_outcome_model_list_clean <- survFMM_object %>%
+      purrr::keep_at(names(survFMM_object)[grepl("final_outcome_model_tidy", names(survFMM_object))]) %>%
+      setNames(., final_outcome_model_rename_list$new) %>%
+      .[order(names(.))]
+
+    # covariance matrix (of outcome models) objects
+    final_outcome_model_covar_mtx_rename_list <- tidyr::tibble(
+      original = paste0("final_outcome_model_cov_mtx_", subgroup_order$k),
+      new = paste0("final_outcome_model_cov_mtx_", subgroup_order$k_clean)) %>%
+      dplyr::arrange(original)
+
+    final_outcome_model_cov_mtx_clean <- survFMM_object %>%
+      purrr::keep_at(names(survFMM_object)[grepl("final_outcome_model_cov_mtx", names(survFMM_object))]) %>%
+      setNames(., final_outcome_model_covar_mtx_rename_list$new) %>%
+      .[order(names(.))]
 
     # new results object
     # combine original list + corrected objects
@@ -94,13 +132,13 @@ clean_subgroup_labels <- function(survFMM_object,
       # original list
       c(survFMM_object %>%
       # drop list elements that required correction
-      purrr::discard_at(names(survFMM_object)[grepl("final_outcome_model_tidy_|final_outcome_model_cov_mtx_|subgroup_assn|final_subgroup_model_tidy|final_df", names(survFMM_object))]),
+      purrr::discard_at(names(survFMM_object)[grepl("final_outcome_model_tidy_|final_outcome_model_cov_mtx_|subgroup_assn|final_subgroup_model|final_subgroup_model_tidy|final_subgroup_model_cov_mtx|final_df", names(survFMM_object))]),
       # corrected objects
-      tibble::lst(subgroup_assn, final_subgroup_model_tidy, final_df),
-      list("final_outcome_model_tidy_1" = pluck(survFMM_object, "final_outcome_model_tidy_2"),
-           "final_outcome_model_tidy_2" = pluck(survFMM_object, "final_outcome_model_tidy_1"),
-           "final_outcome_model_cov_mtx_1" = pluck(survFMM_object, "final_outcome_model_cov_mtx_2"),
-           "final_outcome_model_cov_mtx_2" = pluck(survFMM_object, "final_outcome_model_cov_mtx_1")))
+      tibble::lst(subgroup_assn, final_subgroup_model, final_subgroup_model_tidy, final_subgroup_model_cov_mtx,
+                  final_df),
+      # these objects are fully flipped, no manipulation required, only re-naming, done above
+      final_outcome_model_list_clean,
+      final_outcome_model_cov_mtx_clean)
 
     # order list elements in original order
     aft_fmm_clean <- aft_fmm_clean[names(survFMM_object)]
--- tests/testthat/test-clean_subgroup_labels.R
+++ tests/testthat/test-clean_subgroup_labels.R
@@ -1,119 +1,69 @@
 test_that("clean_subgroup_labels reorders subgroups correctly", {
-  set.seed(1213) # with this seed, subgroup labels require switching
-  aft_fmm_obj <- survFMM(
-    model = "aft-fmm",
-    input_df = sim_data,
-    weights = "iptw_trim97",
-    outc_model_time = "time_to_event_days",
-    outc_model_status = "event_status",
-    outc_model_covars = "tx",
-    covariates_subgroup_model = "covariate_sim_normal",
-    n_inits = 1
-  )
-  cleaned_aft_fmm_obj <- clean_subgroup_labels(aft_fmm_obj, "tx")
+  cleaned_aft_fmm_obj <- clean_subgroup_labels(aft_fmm_obj_k2_req_label_switch, "tx")
 
   # check that subgroup 2 flipped to subgroup 1 in the outcome models
   expect_equal(cleaned_aft_fmm_obj$final_outcome_model_tidy_1 %>%
                  dplyr::filter(term == "tx") %>%
-                 pull(estimate),
-               aft_fmm_obj$final_outcome_model_tidy_2 %>%
+                 dplyr::pull(estimate),
+               aft_fmm_obj_k2_req_label_switch$final_outcome_model_tidy_2 %>%
                  dplyr::filter(term == "tx") %>%
-                 pull(estimate))
+                 dplyr::pull(estimate))
 
   # check that subgroup 2 flipped to subgroup 1 in the covariance matrices
   expect_equal(cleaned_aft_fmm_obj$final_outcome_model_cov_mtx_1,
-               aft_fmm_obj$final_outcome_model_cov_mtx_2)
+               aft_fmm_obj_k2_req_label_switch$final_outcome_model_cov_mtx_2)
 
   # check that subgroup 1 flipped to subgroup 2 in the outcome models
   expect_equal(cleaned_aft_fmm_obj$final_outcome_model_tidy_2 %>%
                  dplyr::filter(term == "tx") %>%
-                 pull(estimate),
-               aft_fmm_obj$final_outcome_model_tidy_1 %>%
+                 dplyr::pull(estimate),
+               aft_fmm_obj_k2_req_label_switch$final_outcome_model_tidy_1 %>%
                  dplyr::filter(term == "tx") %>%
-                 pull(estimate))
+                 dplyr::pull(estimate))
 
   # check that subgroup 1 flipped to subgroup 2 in the covariance matrices
   expect_equal(cleaned_aft_fmm_obj$final_outcome_model_cov_mtx_2,
-               aft_fmm_obj$final_outcome_model_cov_mtx_1)
+               aft_fmm_obj_k2_req_label_switch$final_outcome_model_cov_mtx_1)
 
   # check that assn_subgroup is updated in final_df
-  expect_equal(count(cleaned_aft_fmm_obj$final_df, assn_subgroup) %>%
-                 arrange(desc(assn_subgroup)) %>%
-                 pull(n),
-               count(aft_fmm_obj$final_df, assn_subgroup) %>%
-                 pull(n))
+  expect_equal(dplyr::count(cleaned_aft_fmm_obj$final_df, assn_subgroup) %>%
+                 dplyr::arrange(desc(assn_subgroup)) %>%
+                 dplyr::pull(n),
+               dplyr::count(aft_fmm_obj_k2_req_label_switch$final_df, assn_subgroup) %>%
+                 dplyr::pull(n))
 
   # check subgroup_assn
   expect_equal(cleaned_aft_fmm_obj$subgroup_assn$posterior_prob2,
-               aft_fmm_obj$subgroup_assn$posterior_prob1)
+               aft_fmm_obj_k2_req_label_switch$subgroup_assn$posterior_prob1)
 
   expect_equal(cleaned_aft_fmm_obj$subgroup_assn$posterior_prob1,
-               aft_fmm_obj$subgroup_assn$posterior_prob2)
+               aft_fmm_obj_k2_req_label_switch$subgroup_assn$posterior_prob2)
 
-  expect_equal(count(cleaned_aft_fmm_obj$subgroup_assn, assn_subgroup) %>%
-                 arrange(desc(assn_subgroup)) %>%
-                 pull(n),
-               count(aft_fmm_obj$subgroup_assn, assn_subgroup) %>%
-                 pull(n))
+  expect_equal(dplyr::count(cleaned_aft_fmm_obj$subgroup_assn, assn_subgroup) %>%
+                 dplyr::arrange(desc(assn_subgroup)) %>%
+                 dplyr::pull(n),
+               dplyr::count(aft_fmm_obj_k2_req_label_switch$subgroup_assn, assn_subgroup) %>%
+                 dplyr::pull(n))
 
   # check final_subgroup_model_tidy
   expect_equal(cleaned_aft_fmm_obj$final_subgroup_model_tidy$estimate,
-               -aft_fmm_obj$final_subgroup_model_tidy$estimate)
+               -aft_fmm_obj_k2_req_label_switch$final_subgroup_model_tidy$estimate)
 
   expect_equal(cleaned_aft_fmm_obj$final_subgroup_model_tidy$conf.low,
-               -aft_fmm_obj$final_subgroup_model_tidy$conf.high)
+               -aft_fmm_obj_k2_req_label_switch$final_subgroup_model_tidy$conf.high)
 
   expect_equal(cleaned_aft_fmm_obj$final_subgroup_model_tidy$conf.high,
-               -aft_fmm_obj$final_subgroup_model_tidy$conf.low)
+               -aft_fmm_obj_k2_req_label_switch$final_subgroup_model_tidy$conf.low)
 })
 
 test_that("clean_subgroup_labels does not reorder if already correct", {
-  set.seed(007) # with this seed, subgroup labels DO NOT require switching
-  aft_fmm_obj <- survFMM(
-    model = "aft-fmm",
-    input_df = sim_data,
-    weights = "iptw_trim97",
-    outc_model_time = "time_to_event_days",
-    outc_model_status = "event_status",
-    outc_model_covars = "tx",
-    covariates_subgroup_model = "covariate_sim_normal",
-    n_inits = 1
-  )
-  cleaned_aft_fmm_obj <- clean_subgroup_labels(aft_fmm_obj, "tx")
+  cleaned_aft_fmm_obj <- clean_subgroup_labels(aft_fmm_obj_k2_labels_no_switch, "tx")
 
-  expect_equal(aft_fmm_obj, cleaned_aft_fmm_obj)
-})
-
-test_that("clean_subgroup_labels throws error for more than 2 subgroups", {
-  set.seed(1213)
-  aft_fmm_obj_k3 <- survFMM(
-    model = "aft-fmm",
-    input_df = sim_data,
-    weights = "iptw_trim97",
-    outc_model_time = "time_to_event_days",
-    outc_model_status = "event_status",
-    outc_model_covars = "tx",
-    k = 3,
-    covariates_subgroup_model = "covariate_sim_normal",
-    n_inits = 1
-  )
-
-  expect_error(clean_subgroup_labels(aft_fmm_obj_k3, "tx"),
-               "Subgroup label alignment is currently only available for 2 subgroups.")
+  expect_equal(aft_fmm_obj_k2_labels_no_switch, cleaned_aft_fmm_obj)
 })
 
 test_that("clean_subgroup_labels handles missing tx_term", {
   set.seed(1213)
-  aft_fmm_obj <- survFMM(
-    model = "aft-fmm",
-    input_df = sim_data,
-    weights = "iptw_trim97",
-    outc_model_time = "time_to_event_days",
-    outc_model_status = "event_status",
-    outc_model_covars = "tx",
-    covariates_subgroup_model = "covariate_sim_normal",
-    n_inits = 1
-  )
 
-  expect_error(clean_subgroup_labels(aft_fmm_obj, "abc"), "^Check that the `tx_term` input")
+  expect_error(clean_subgroup_labels(aft_fmm_obj_k2, "abc"), "^Check that the `tx_term` input")
 })
